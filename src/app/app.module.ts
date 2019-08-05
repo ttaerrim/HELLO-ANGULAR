346098b9927d5db35be653b6fbbabfca6b019c75
leetaerim <ltr0121@naver.com>
2019-08-05 18:11:43 +0900

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SectionComponent } from './section/section.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent,
+    HeaderComponent,
+    FooterComponent,
+    SectionComponent,
   ],
   imports: [
     BrowserModule
